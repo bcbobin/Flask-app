8b5efd678cfb5c81311ddc15b76d24862353f203
Bogdan Bobin <bogdan3964@hotmail.com>
2019-01-25 09:49:55 -0500

    Before removing pulling date from form

---
+++ Flask/g_wifi/app.py
@@ -0,0 +1,82 @@
+
+import os, tempfile, datetime
+import main
+import execute, util
+import subprocess  #TODO(if importing other functions and not converting to python fully) - use to run php possibly 
+from flask import Flask, jsonify, request, render_template, flash, redirect, send_file, send_from_directory
+from passlib.hash import sha256_crypt #to be used for password encryption 
+#from flask_socketio import SocketIO
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+app.secret_key = '▒R+▒4w▒▒Y}4*ҟ▒'        #use command to generate python -c 'import os; print(os.urandom(16))'
+#UPLOAD_FOLDER = '/tmp'
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+#########Jan 25th##########
+#TODO - add back dropdown for date, replace date logic (7 Days -> Friday next week iff Wednesday or after )
+#TODO - lock site and authenticate (low prio)
+#TODO - general error testing (bug hunt)
+
+#comunicate with other script and pull post values 
+@app.route('/new', methods=['POST'])
+def data():
+    s_username = request.form['login']
+    s_password = request.form['password']
+    s_email = request.form['sponsoremail'].lower()
+    #TODO - is an economical check required for sponser_email
+    ritm_num = request.form['ritm']
+    duration = request.form['length']
+    if((s_username == "wifiadmin" and s_password == "4Wifi_Aut@mate") or (s_username == "servicedesk" and s_password == "SDesk_Aut@mate")):
+        auth = main.authorize(util.admin(), util.adminp())
+    else:
+        auth = main.authorize(s_username, s_password)   
+    if auth == -1:
+        flash("Authorization Failed, try again")
+    else:
+        data = main.record_retrieve(ritm_num)
+        if data['ritm_not_found'] == "true":
+            flash('Record could not be retrieved')
+        elif data['fail'] == "true":
+            flash("RITM record returned unexpected value. Check RITM number!")
+        elif data['email_invalid'] == "true":
+            flash("guest user email format incorrect, check RITM form!")
+        elif data['approval'] == "needed" and (s_username == "wifiadmin" or s_username == "servicedesk"):
+            flash("time requested needs Networking Approval(7+ days)!" )
+        elif data['invalid_date'] == "true":
+            flash("date on request is invalid, check RITM form")
+        else:
+            guest_pass = main.pass_gen()
+            counter = 0
+            if((s_username == "wifiadmin" and s_password == "4Wifi_Aut@mate") or (s_username == "servicedesk" and s_password == "SDesk_Aut@mate")):
+                s_username = util.admin()
+                s_password = util.adminp()
+            while(counter < 3):
+                status = execute.execute(data['guest_email'], data['guest_name'], data['company'], data['guest_phone'], str(data['duration_seconds']), data['s_dept'], s_email,s_username,s_password,guest_pass,data['details'])
+                if (status == -2):
+                    counter = 1 + counter
+                if (status == -1):                                          #checks for controller auth, kept in for safety 
+                    flash("Invalid Username or Password")   
+                    break
+                if (status == -3):
+                    flash("Account already exists, search for " + data['guest_email'] + "to see account information" )
+                    break
+                if (status == 0):
+                    flash("Successful addition, guest user information: ")
+                    flash("Username: " +data['guest_email']+ ' \n\n     Password: '+ guest_pass) 
+                    flash(' \n\n Guest Company: '+data['company']+'     \n\n Guest Email: '+data['guest_email'])
+                    flash(' \n\n Guest Phone: '+data['guest_phone']+'    \n\n Sponsor Email: '+s_email)
+                    flash(' \n\n Start Date: '+ data['start_date'] + '    \n\n Expires on: '+  data['end_date'])
+                    break
+            if(counter == 3):
+                flash("Controller is busy, try again in 5 minutes")
+            else:
+                main.update_records(ritm_num)
+    return redirect('/', code = 302)      
+ 
+if __name__ == '__main__':
+   app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
--- Flask/g_wifi/main.py
+++ Flask/g_wifi/main.py
@@ -1,8 +1,8 @@
 # Created by Bogdan Bobin
-# Last Updated August 20/18 
-# Version 0.7.0
+# Last Updated January 22/19 
+# Version 0.7.9
 ################################################################
-#!/usr/bin/env python3.4
+
 #print("conntect")
 import cgi, cgitb 
 import sys , traceback
@@ -13,105 +13,157 @@ import sys
 import json
 import urllib
 #for reading excel data, pandas requires xlrd
-import pandas as pd
-import xlrd
+#import pandas as pd
+#import xlrd
 #for date and time 
-import datetime
+from datetime import datetime
+import string
+import random 
 import smtplib 
 from email.mime.text import MIMEText 
 
-
+    #TODO- Tho - web html bug, looks weird on a 17 inch monitor 
 def authorize(username, password):
-  unauthorized = True
-  
-  while (unauthorized):
+    unauthorized = True
+
+    while (unauthorized):
 
     #for authentication covering all future urllib.request uses when sending queries to ServiceNow
-    url = "https://economical.service-now.com/api/now/v2/ui/table/sc_req_item" 
-    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-    top_url = "https://economical.service-now.com/"
-    password_mgr.add_password(None, top_url, username , password)
-    handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-    opener = urllib.request.build_opener(handler)
-    try:
-      opener.open(url)
-    except urllib.request.HTTPError as err:
-      #stop code if auth fails
-      if (err.code == 401):
-        print("Auth Error")
-        sys.stdout.flush()
-      
-  
-    unauthorized = False 
-    urllib.request.install_opener(opener)
-    
-def record_retrieve(req_number):
+        url="https://economical.service-now.com/api/now/v2/ui/table/sc_req_item" 
+        password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+        top_url = "https://economical.service-now.com/"
+        password_mgr.add_password(None, top_url, username , password)
+        handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib.request.build_opener(handler)
+        try:
+          opener.open(url)
+        except urllib.request.HTTPError as err:
+          #stop code if auth fails
+          if (err.code == 401):
+            return -1
+          
 
+        unauthorized = False 
+        urllib.request.install_opener(opener)
+    return 0
+
+def record_retrieve(req_number):
+    curr_date = datetime.now()                             
+    info = {}
+    info['ritm_not_found'] = "false"
+    info['fail'] = "false"
+    info['approval'] = "false"
+    info['email_invalid'] = "false"
+    info['invalid_date'] = "false"
     #retreive item sysid by sending a query
     idurl = "https://economical.service-now.com/sc_req_item.do?JSONv2&sysparm_action=getRecords&sysparm_query=active=true^GOTOnumberLIKE" + str(req_number) + "^ORDERBYnumber&displayvariables=true&displayvalue=true"
     req = urllib.request.Request(idurl)
     req = urllib.request.urlopen(idurl)
     data = req.read().decode()
     #print(data)
+    data = json.loads(data)
+    data = data['records']
+    try:
+        data = data[0]
+    except(IndexError):
+        info['ritm_not_found'] = "true"
+        return info
+    child = data['variables']
+    #print(child)
+    try:                                        #try to pull all relevent data, if fails, wrong type of RITM or ServiceNow layout changed
+        sponsor_name = child[0]['children'][0]['value']
+        recipient = child[0]['children'][1]['value']
+        department = child[0]['children'][2]['value']
+        email = child[4]['children'][2]['children'][0]['value']
+        start_date = child[5]['children'][0]['value']
+        end_date = child[5]['children'][2]['value']
+        info['start_date'] = start_date                          #add the values to dict before they get changed 
+        info['end_date'] = end_date
+        start_date = start_date.rsplit(' ', 1)[0]
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = end_date.rsplit(' ', 1)[0]
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        phone = child[4]['children'][0]['children'][0]['value']
+        details = child[6]['children'][0]['value']
+    except(KeyError, IndexError):
+        info['fail'] = "true"
+        return info
+    try:
+        company = email.rsplit('@', 1)[1]
+        company = company.rsplit('.', 1)[0]
+    except(IndexError):
+        info['email_invalid'] = "true"
+        return info
+    
+    print ("Opened by : ", sponsor_name)    
+    print ("On behalf of: ", recipient) 
+    print ("Department: ", department)  
+    print("email: " ,email)
+    print ("phone: ",phone)
+    print ("company: ", company)
+    print ("Start: ",start_date)
+    print ("End: ", end_date)
+    print("Details: ", details)
+    print ("Curr date: ", curr_date)
+    
+    info['guest_email'] = email.lower() 
+    info['s_dept'] = department 
+    info['sponser_name'] = sponsor_name 
+    info['guest_name'] = recipient  
+    info['guest_phone'] = phone
+    info['company'] = company 
+    info['details'] = details 
+    
+    
+    days = (end_date - start_date)
+    seconds = days.total_seconds()
+    if(seconds > 604800):
+        info['approval'] = "needed"
+    if(seconds == 0):
+        seconds = 86400
+    actual_seconds = end_date - curr_date
+    if (actual_seconds < 0):
+        info['invalid_date'] = "true"
+    actual_seconds = actual_seconds.total_seconds()
+    info['duration_seconds'] = int(actual_seconds)
+    print("Seconds: ", actual_seconds, "\n")
+    
+    return info
+
+    
+def pass_gen(size = 8, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def update_records(ritm):
+    #possibly call auth from here with other credientials 
+    idurl = "https://economical.service-now.com/sc_req_item.do?JSONv2&sysparm_action=getKeys&sysparm_query=active=true^GOTOnumberLIKE"+ str(ritm) +"^ORDERBYnumber&displayvariables=true&displayvalue=true"
+    req = urllib.request.Request(idurl)
+    req = urllib.request.urlopen(idurl)
+    data = req.read().decode()
     sysrecords = json.loads(data)
     #print(sysrecords)
-    sysid = sysrecords['records']
+    sysid = sysrecords.get("records")
     sysid = sysid[0]
-    #print(sysid)
-    print("\n")
-    child = sysid['variables']
-    print(child)
-    print("\n")
-    recipient = child[0]['children'][1]['value']
-    print ("On behalf of: ", recipient)
-    print("\n")
-    department = child[0]['children'][2]['value']
-    print ("Department: ", department)
-    category = child[1]
-    print ("category: ",category)
-    print("\n")
-    child3 = child[2]
-    print ("child3: ",child3)
-    print("\n")
-    child4 = child[3]
-    print ("child4: ",child4, len(child4))
-    print("\n")
-    email = child[4]['children'][2]['children'][0]['value']
-    print("email: " ,email)
-    phone = child[4]['children'][0]['children'][0]['value']
-    print ("phone: ",phone,)
-    print("\n")
-    child6 = child[5]
-    print ("child6: ",child6, len(child6))
-    print("\n")
-    child7 = child[6]
-    print ("child7: ", child7)
-    print("\n")
-    
-    #getting info from database as a dictionary using sysid
-    try:
-      geturl = "https://economical.service-now.com/sc_req_item.do?JSONv2&sysparm_sys_id=" + str(sysid['sys_id']) +"&displayvalue=true"
-    except IndexError:
-      print( "REQ record: "+ str(asset_tag[i]) + " not found" )
-      
+    print(sysid)
+    new = {"state" : "1"}                               #4=Closed Incomplete, 3= Closed, 2= Work in Progress, 1= Open, -5=Pending
+    #updating records with html PUT request
+    updateurl = 'https://economical.service-now.com/api/now/v1/table/sc_req_item/' + str(sysid)
+    update = urllib.request.Request(updateurl, data=bytes(str(new), 'utf-8'), method='PUT')
+    update.add_header('Content-Type', 'application/json;charset=utf-8')
+    update.add_header('Accept', 'application/json')
+    urllib.request.urlopen(update)
 
-    formreq = urllib.request.Request(geturl)
-    formreq = urllib.request.urlopen(geturl)
-    str_raw = formreq.read().decode()
-    raw = json.loads(str_raw)
-    values = raw['records']
-    info = values[0]
-  
-    #for reference of response
-    test = open("asset_data.txt", "w")
-    test.write(str(values[0]))
-    test.close()
-    
-    
-authorize("irg", "N0vember24")
-record_retrieve("RITM0072297")
-    
-    
-    
-    
-    
+
+
+
+
+#authorize("irg", "")
+#record_retrieve("RITM0072297")
+#TODO- ask what credectials to use when authorizing and closing the request
+
+
+
+
+
+
